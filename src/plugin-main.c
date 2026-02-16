e23a74b3b0611dcddf6d86a5545f1d9db6f6ac00
BoomSoMuch <46388061+BoomSoMuch@users.noreply.github.com>
2026-02-16 11:31:45 -0500

    Update plugin-main.c

--- src/plugin-main.c
+++ src/plugin-main.c
@@ -1,34 +1,106 @@
-/*
-Plugin Name
-Copyright (C) <Year> <Developer> <Email Address>
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program. If not, see <https://www.gnu.org/licenses/>
-*/
-
 #include <obs-module.h>
-#include <plugin-support.h>
+#include <string>
 
-OBS_DECLARE_MODULE()
-OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
+// ----------------------------------------------------------------------------
+// 1. ZOOM PARTICIPANT SOURCE
+// ----------------------------------------------------------------------------
 
-bool obs_module_load(void)
-{
-	obs_log(LOG_INFO, "plugin loaded successfully (version %s)", PLUGIN_VERSION);
-	return true;
+struct ZoomParticipantData {
+    std::string name;
+};
+
+// Returns the display name of the source
+const char* zp_get_name(void* type_data) {
+    return "Zoom Participant";
 }
 
-void obs_module_unload(void)
-{
-	obs_log(LOG_INFO, "plugin unloaded");
+// Called when the user adds the source
+void* zp_create(obs_data_t* settings, obs_source_t* source) {
+    ZoomParticipantData* data = new ZoomParticipantData();
+    return data;
+}
+
+// Called when the source is removed
+void zp_destroy(void* data) {
+    ZoomParticipantData* p = static_cast<ZoomParticipantData*>(data);
+    delete p;
+}
+
+// A dummy render function (draws a placeholder box so you can see it)
+void zp_video_render(void* data, gs_effect_t* effect) {
+    // This just clears the source area to a dark Zoom-blue color for the demo
+    gs_clear(GS_CLEAR_COLOR, (struct vec4*) & (struct vec4){0.17f, 0.5f, 1.0f, 1.0f}, 0.0f, 0);
+}
+
+// Defines the source properties
+struct obs_source_info zoom_participant_info = {};
+
+void init_zoom_participant() {
+    zoom_participant_info.id = "zoom_participant_source";
+    zoom_participant_info.type = OBS_SOURCE_TYPE_INPUT;
+    zoom_participant_info.output_flags = OBS_SOURCE_VIDEO;
+    zoom_participant_info.get_name = zp_get_name;
+    zoom_participant_info.create = zp_create;
+    zoom_participant_info.destroy = zp_destroy;
+    zoom_participant_info.video_render = zp_video_render;
+}
+
+
+// ----------------------------------------------------------------------------
+// 2. ZOOM SCREENSHARE SOURCE
+// ----------------------------------------------------------------------------
+
+struct ZoomScreenData {
+    std::string name;
+};
+
+const char* zs_get_name(void* type_data) {
+    return "Zoom Screenshare";
+}
+
+void* zs_create(obs_data_t* settings, obs_source_t* source) {
+    ZoomScreenData* data = new ZoomScreenData();
+    return data;
+}
+
+void zs_destroy(void* data) {
+    ZoomScreenData* p = static_cast<ZoomScreenData*>(data);
+    delete p;
+}
+
+void zs_video_render(void* data, gs_effect_t* effect) {
+    // Clears to a green color to distinguish it from participants
+    gs_clear(GS_CLEAR_COLOR, (struct vec4*) & (struct vec4){0.0f, 0.8f, 0.2f, 1.0f}, 0.0f, 0);
+}
+
+struct obs_source_info zoom_screenshare_info = {};
+
+void init_zoom_screenshare() {
+    zoom_screenshare_info.id = "zoom_screenshare_source";
+    zoom_screenshare_info.type = OBS_SOURCE_TYPE_INPUT;
+    zoom_screenshare_info.output_flags = OBS_SOURCE_VIDEO;
+    zoom_screenshare_info.get_name = zs_get_name;
+    zoom_screenshare_info.create = zs_create;
+    zoom_screenshare_info.destroy = zs_destroy;
+    zoom_screenshare_info.video_render = zs_video_render;
+}
+
+
+// ----------------------------------------------------------------------------
+// PLUGIN REGISTRATION
+// ----------------------------------------------------------------------------
+
+OBS_DECLARE_MODULE()
+OBS_MODULE_USE_DEFAULT_LOCALE("obs-zoom-connector", "en-US")
+
+bool obs_module_load(void) {
+    // Initialize our structure data
+    init_zoom_participant();
+    init_zoom_screenshare();
+
+    // Register the sources with OBS
+    obs_register_source(&zoom_participant_info);
+    obs_register_source(&zoom_screenshare_info);
+
+    return true;
 }
