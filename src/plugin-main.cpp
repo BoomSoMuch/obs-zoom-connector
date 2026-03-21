5bee3f8182f2b7bed4297767b8f3add50c3c2826
BoomSoMuch <46388061+BoomSoMuch@users.noreply.github.com>
2026-03-20 23:51:17 -0400

    Update plugin-main.cpp

--- src/plugin-main.cpp
+++ src/plugin-main.cpp
@@ -1,10 +1,15 @@
+// 1. Windows headers MUST come first for the SDK to recognize HWND, UINT64, etc.
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <tchar.h>
+
+// 2. Zoom SDK headers
 #include "zoom_sdk.h"
 #include "meeting_service_interface.h"
 #include "auth_service_interface.h"
-// Corrected path based on your 'meeting_service_components' folder
 #include "meeting_service_components/meeting_recording_interface.h"
+
 #include <iostream>
-#include <tchar.h> // For _T() support
 
 using namespace ZOOMSDK;
 
@@ -17,7 +22,6 @@ public:
     void onZoomIdentityExpired() override {}
     void onZoomAuthIdentityExpired() override {}
 #if defined(WIN32)
-    // Mandatory for Windows interface
     void onNotificationServiceStatus(SDKNotificationServiceStatus status, SDKNotificationServiceError error) override {}
 #endif
 };
@@ -32,10 +36,8 @@ public:
     void onAICompanionActiveChangeNotice(bool bActive) override {}
     void onMeetingTopicChanged(const zchar_t* sTopic) override {}
     void onMeetingFullToWatchLiveStream(const zchar_t* sLiveStreamUrl) override {}
-    // Added missing network status handler
     void onUserNetworkStatusChanged(MeetingComponentType type, ConnectionQuality level, unsigned int userId, bool uplink) override {}
 #if defined(WIN32)
-    // Mandatory Windows callback
     void onAppSignalPanelUpdated(IMeetingAppSignalHandler* pHandler) override {}
 #endif
 };
@@ -44,7 +46,7 @@ public:
 class ZoomRecordingListener : public IMeetingRecordingCtrlEvent {
 public:
     void onRecordingStatus(RecordingStatus status) override {}
-    void onCloudRecordingStatus(RecordingStatus status) override {}
+    void onCloudRecordingStatus(RecordingStatus status) override { std::cout << "Cloud Rec Status: " << status << std::endl; }
     void onRecordPrivilegeChanged(bool bCanRec) override {}
     void onLocalRecordingPrivilegeRequestStatus(RequestLocalRecordingStatus status) override {}
     void onRequestCloudRecordingResponse(RequestStartCloudRecordingStatus status) override {}
@@ -54,7 +56,6 @@ public:
     void onEnableAndStartSmartRecordingRequested(IRequestEnableAndStartSmartRecordingHandler* handler) override {}
     void onSmartRecordingEnableActionCallback(ISmartRecordingEnableActionHandler* handler) override {}
 #if defined(WIN32)
-    // Mandatory Windows callbacks for local recording
     void onRecording2MP4Done(bool bsuccess, int iResult, const zchar_t* szPath) override {}
     void onRecording2MP4Processing(int iPercentage) override {}
     void onCustomizedLocalRecordingSourceNotification(ICustomizedLocalRecordingLayoutHelper* layout_helper) override {}
@@ -65,22 +66,23 @@ public:
 ZoomAuthListener authListener;
 ZoomMeetingListener meetingListener;
 ZoomRecordingListener recordingListener;
+IZoomSDK* g_pSDKInst = nullptr; // Rename to avoid confusion with the function name
 
 extern "C" __declspec(dllexport) bool InitializeSDK(const char* jwt) {
     InitParam initParam;
     initParam.strWebDomain = _T("https://zoom.us");
     
-    IZoomSDK* SDKInst = nullptr;
-    SDKError err = CreateSDKInst(&SDKInst);
+    // Use the global pointer
+    SDKError err = CreateSDKInst(&g_pSDKInst);
+    if (err != SDKERR_SUCCESS || !g_pSDKInst) return false;
+
+    err = g_pSDKInst->InitSDK(initParam);
     if (err != SDKERR_SUCCESS) return false;
 
-    err = SDKInst->InitSDK(initParam);
-    if (err != SDKERR_SUCCESS) return false;
-
-    IAuthService* pAuth = SDKInst->GetAuthService();
+    IAuthService* pAuth = g_pSDKInst->GetAuthService();
     if (pAuth) pAuth->SetEvent(&authListener);
 
-    IMeetingService* pMeeting = SDKInst->GetMeetingService();
+    IMeetingService* pMeeting = g_pSDKInst->GetMeetingService();
     if (pMeeting) pMeeting->SetEvent(&meetingListener);
 
     return true;
